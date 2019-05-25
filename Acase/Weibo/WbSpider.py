69424e19c60eb525427f637622cd18efcd7fbcd6
bendan <ada20112012@gmail.com>
2019-05-25 23:49:08 +0700

    [微博]   使用selenium抓取微博内容

---
+++ Acase/Weibo/WbSpider.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+class WeiboSpider:
+
+    def __init__(self):
+        print('调用构造函数\n')
+        self.url = 'https://www.weibo.com/dengpan22?is_all=1'
+
+    def __del__(self):
+        print('\n对象不再使用，析构')
+
+    def start(self):
+        print('开始抓取微博内容')
+        option = webdriver.ChromeOptions()
+        # option.add_argument('headless')
+
+        driver = webdriver.Chrome(
+            executable_path='./chromedriver',
+            options=option
+        )
+
+        # 打开网站
+        driver.get(self.url)
+
+        # 等待网页加载完成
+        timeout = 30
+        wb_content = WebDriverWait(driver, timeout).until(
+            lambda e: e.find_elements_by_xpath('//div[@class="WB_text W_f14"]'))
+
+        if len(wb_content) > 0:
+            for wb_item in wb_content:
+                print(wb_item.text)
+
+
+if __name__ == '__main__':
+    wbspider = WeiboSpider()
+    wbspider.start()
