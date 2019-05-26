47f879066793fff1bfab2bb67ef6076ae9e8fa1d
bendan <ada20112012@gmail.com>
2019-05-26 10:22:03 +0700

    [微博]  根据xpath查找到的Card，取出里面的细分内容进行存储

--- Acase/Weibo/WbSpider.py
+++ Acase/Weibo/WbSpider.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
 from DB import DB,DbType
 
 
@@ -30,19 +33,37 @@ class WeiboSpider:
         # 等待网页加载完成
         timeout = 30
         wb_content = WebDriverWait(driver, timeout).until(
-            lambda e: e.find_elements_by_xpath('//div[@class="WB_text W_f14"]'))
+            lambda e: e.find_elements_by_xpath('//div[@class="WB_cardwrap WB_feed_type S_bg2 WB_feed_like "]'))
+
+        #element = driver.find_elements_by_xpath('//div[@class="WB_text W_f14"]')
 
         if len(wb_content) > 0:
             data = []
             for wb_item in wb_content:
                 #print(wb_item.text)
-                data.append({'content':wb_item.text})
+                # 取发布时间
+                time = wb_item.find_element_by_xpath('.//div[@class="WB_from S_txt2"]/a')
+                print(time.get_property('title'))
+                # 发布设备
+                form = wb_item.find_element_by_xpath('.//div[@class="WB_from S_txt2"]/a[@action-type="app_source"]')
+                print(form.text)
+                # 发布内容
+                content = wb_item.find_element_by_xpath('.//div[@class="WB_text W_f14"]')
+                print(content.text)
+
+                data.append({
+                    'time': time.get_property('title'),
+                    'from': form.text,
+                    'content':content.text
+                })
 
             self.save(data)
 
     def save(self, data):
-        db = DB()
-        db.save(DbType.MONGO, data)
+        if len(data) > 0:
+            db = DB()
+            db.save(DbType.MONGO, data)
+
 
 
 
